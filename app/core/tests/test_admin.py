c7754fba3ca4bf53a7669f5f8f5511e5081fbd1c
LibGdx <libgdxengine@gmail.com>
2021-09-11 23:38:22 +0200

    Upload admin to support custom user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,4 +1,4 @@
-from django.test import TestCase , Client
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
@@ -20,7 +20,20 @@ class AdminSiteTest(TestCase):
 
     def test_users_listed(self):
         """test that users are listed in users page"""
-        url = reverse("admin:core_user_change_list")
+        url = reverse('admin:core_user_changelist')
         response = self.client.get(url)
         self.assertContains(response, self.user.name)
         self.assertContains(response, self.user.email)
+
+    def test_user_change_page(self):
+        """test that user edit page works as expected"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # admin/core/user/id_of_user
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_page(self):
+        """test creating new user page"""
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
