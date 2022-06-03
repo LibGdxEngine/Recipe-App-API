a2b72493a5d9e532a0bcf2c4fc96dabb240f831f
LibGdx <libgdxengine@gmail.com>
2022-06-04 00:00:41 +0200

    added postgress db

--- app/core/models.py
+++ app/core/models.py
@@ -1,29 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser , BaseUserManager, PermissionsMixin, \
-                                                        PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, \
+    BaseUserManager, PermissionsMixin
+
 
 class UserManager(BaseUserManager):
 
-    def create_user(self , email , password=None , **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """creates and saves new users """
         if not email:
             raise ValueError('User must have an email address')
-        user = self.model(email=email.lower() , **extra_fields)
+        user = self.model(email=email.lower(), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
         return user
 
-    def create_superuser(self , email , password):
+    def create_superuser(self, email, password):
         """creates and saves a new superuser"""
-        user = self.create_user(email=email , password=password)
+        user = self.create_user(email=email, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
         return user
 
-class User(AbstractBaseUser , PermissionsMixin):
+
+class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
